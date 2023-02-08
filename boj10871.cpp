4cf36e44c86bf9351e69e875d4c50121ff4aff65
 NohYeongHun <dkzndk1100@naver.com>
2023-02-08 09:02:01 +0900

    표준입출력,  boj10871 test

---
+++ boj10871.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x;
+    cin >> n >> x ;
+    int array[n];
+    for(int i = 0; i < n; i ++){
+        cin >> array[i];
+    }
+
+    string result = "";
+
+    for(int i = 0; i < n; i ++){
+        if (array[i] < x){
+            result += to_string(array[i]);
+            result += " ";
+        }
+    }
+
+    result.resize(result.size() - 1);
+    cout << result;
+    
+    return 0;
+}
--- standardInput.cpp
+++ standardInput.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int main(){
+int main(void){
     string s;
     cout << "input : ";
     cin >> s;
     cout << "s is " << s;
-
     return 0;
+
 }
