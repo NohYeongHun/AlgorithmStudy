52597a8f7c5f0f6ab3573329bc4aeccd94145328
 NohYeongHun <dkzndk1100@naver.com>
2023-02-11 22:56:56 +0900

    LinkedList 조금더 개선

--- linkListPractice/linkList.cpp
+++ linkListPractice/linkList.cpp
@@ -33,12 +33,24 @@ void insert(int addr, int num){
 }
 
 void erase(int addr){
-    dat[addr] = -1;
     int prev = pre[addr];
     int next = nxt[addr];
+    // 앞에 같이 존재한다면?
+    if(next != -1) pre[next] = prev;
+    // prev의 next 값은 next이다.
     nxt[prev] = next;
+    // next의 prev 값음 prev이다.
     pre[next] = prev;
 }
+
+void insert_test(){
+    traverse();
+    insert(5, 10);
+    insert(6, 15);
+    traverse();
+}
+
+void erase
 
 int main(void){
     fill(pre, pre+MX, -1);
@@ -48,10 +60,8 @@ int main(void){
     nxt[0] = 2, nxt[1] = 4, nxt[2] = 1, nxt[4] = 5;
     dat[0] = -1, dat[1] = 65, dat[2] = 13, dat[4] = 21, dat[5] = 17;
     pre[5] = 4, pre[4] = 1, pre[1] = 2, pre[2] = 0;
-
-    traverse();
-    insert(5, 10);
-    traverse();
+    insert_test();
+    
     erase(2);
     traverse();
     return 0;
