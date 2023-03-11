18426ae48c83985b918daa5c5859fa45a30ca34e
 NohYeongHun <dkzndk1100@naver.com>
2023-03-11 12:47:17 +0900

    11강 예제 주석 추가

--- c++grammar/chapter11/0/exam.cpp
+++ c++grammar/chapter11/0/exam.cpp
@@ -11,6 +11,7 @@ int main()
 {   
     // 초기화
     Vec<int> v1(2, 0);
+    // 범위 초과로 grow()함수 호출 => 배열크기 2배로 증가 =>  기존요소 복사 및 새로운 값 넣기. =>  uncreate() 함수 호출 => 
     v1.push_back(10);
     v1.push_back(15);
     v1.push_back(20);
