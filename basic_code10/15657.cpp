df08989c9966f76c7087108e26b332fd961c063a
 NohYeongHun <dkzndk1100@naver.com>
2023-03-27 06:32:24 +0900

    백트래킹 연습문제 15657.cpp

---
+++ basic_code10/15657.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int M;
+int board[10];
+vector<int> v;
+
+void dfs(int N, int M, int s)
+{
+    if (M == 0)
+    {
+        for (vector<int>::iterator iter = v.begin(); iter != v.end(); iter++)
+        {
+            cout << *iter << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = s; i < N; i++)
+    {
+        v.push_back(board[i]);
+        dfs(N, M - 1, i);
+        v.pop_back();
+    }    
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> board[i];
+    }
+
+    sort(board, board + N);
+    dfs(N, M, 0);
+    
+
+
+}
