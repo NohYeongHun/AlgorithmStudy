3de30d85643930a4b00b958ec6455571c12f6d18
 NohYeongHun <dkzndk1100@naver.com>
2023-04-05 20:09:42 +0900

    시뮬레이션 14891.cpp 해결

--- basic_code11/14891.cpp
+++ basic_code11/14891.cpp
@@ -7,22 +7,7 @@ string gear[5];
 int K;
 int number[101];
 int direction[101];
-/*
-    N 극은 0 S극 1
-    gear => 0번 부터
-    톱니바퀴의 3번쨰 인덱스가 12시
-    gear[idx][2]
 
-    0 = > 4
-    1 => 0, 4
-    2 => 0, 4
-    3 => 0
-
-    회전한 톱니바퀴와 서로 맞닿은 톱니의 극이 다르다면, B는 A가 회전한 방향과 반대방향으로 회전
-    반시계는 앞에가 빠짐
-    반시계는 뒤에 0번 인덱스가 pop_front, push_back
-    시계는 pop_back, push_front
-*/
 
 void rotate(int number, int dir)
 {
@@ -30,21 +15,35 @@ void rotate(int number, int dir)
 
     // 반시계
     if (dir == -1)
-    {  
-        char start = q.front(); q.pop_front();
+    {
+        char start = *(q.begin()); q.pop_front();
         q.push_back(start);
-        string temp (q.begin(), q.end());
+        string temp(q.begin(), q.end());
         gear[number - 1] = temp;
     }
     // 시계
     else if (dir == 1)
     {
-        char end = q.back(); q.pop_back();
+        char end = *(q.end() -1); q.pop_back();
         q.push_front(end);
-        string temp (q.begin(), q.end());
+        string temp(q.begin(), q.end());
         gear[number - 1] = temp;
     }
 }
+
+// curNumber와 compNumber의 톱니바퀴를 비교하여 극이 같으면 false, 다르면 true => 회전
+bool rotationCheck(int curNumber, int compNumber, int curIdx, int compIdx)
+{
+
+    char curMagnet = gear[curNumber - 1][curIdx];
+    char compMagnet = gear[compNumber - 1][compIdx];
+
+    if (curMagnet != compMagnet)
+        return true;
+
+    return false;
+}
+
 
 int main()
 {
@@ -58,46 +57,56 @@ int main()
 
     for (int i = 0; i < K; ++i)
     {
-        cin >> number[i] >> direction[i];    
+        cin >> number[i] >> direction[i];
     }
 
-    for (int i = 0; i <K; ++i)
+    for (int i = 0; i < K; ++i)
     {
         int curNumber = number[i];
         int dir = direction[i];
 
-        /*
-            1. 회전할 Gear의 번호를 확인
-            2. 회전하기전 Gear의 상태와, 방향 알기
-            3. 회전 전 Gear와 연관된 톱니바퀴 찾기
-            0 => 1[0]
-            1 => 0[4], 2[0]
-            2 => 1[4], 3[0]
-            3 => 2[4]
-            4. 만약 연관된 톱니바퀴의 맞닿은 부분이 서로 다른 극이면 연관된 톱니바퀴 회전.
-            5. 회전된 톱니바퀴와 연관된 다른 부분도 확인 => 3번으로 돌아감.
-            6. 연관된 톱니바퀴가 모두 회전
-            7. 이제 회전하기로 했던 톱니바퀴 회전.
+        
+        // 회전 톱니바퀴 번호와 톱니바퀴 방향 저장
+        vector<pair<int, int> > savedRotate;
+        savedRotate.push_back({curNumber, dir});
 
-            
-        */ 
-        string temp = gear[curNumber - 1];
+        // 회전 방향
+        int rightDir = dir;
+        int leftDir = dir;
 
-        if (curNumber == 1)
+        // 왼쪽 회전
+        for (int i = curNumber; i > 1 && rotationCheck(i, i - 1, 6, 2) == true; --i)
         {
-
+            leftDir = leftDir == 1 ? -1 : 1;
+            savedRotate.push_back({i - 1, leftDir });
         }
 
-        // 회전
-        rotate(gear[curNumber - 1], dir);
-
-        if (curNumber == 1)
+        // 오른쪽 회전
+        for (int i = curNumber; i < 4 && rotationCheck(i, i + 1, 2, 6) == true; ++i)
         {
-            
+            rightDir = rightDir == 1 ? -1 : 1;
+            // 반대 회전방향을 넣어줘야함.
+            savedRotate.push_back({ i + 1,  rightDir});
         }
+          
 
-
+        // 회전할 톱니바퀴 회전
+        for (const pair<int, int>& p : savedRotate)
+        {
+            rotate(p.first, p.second);
+        }
+        
     }
-    
+    int ans = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (gear[i][0] == '1')
+        {
+            ans += pow(2, i);
+        }
+    }
+
+    cout << ans;
+
 
 }
